102d361f7fd36c2792b9ffcf3b18466b7f237e14
ismailtapcan <112905664+tapcanismail@users.noreply.github.com>
2022-09-23 01:09:36 +0300

    Update egzersiz.js

--- egzersiz.js
+++ egzersiz.js
@@ -23,9 +23,9 @@ function readLine() {
 }
 
 /**
-*   Return the second largest number in the array.
-*   @param {Number[]} nums - An array of numbers.
-*   @return {Number} The second largest number in the array.
+*
+*  
+*  
 **/
 function getSecondLargest(nums) {
     // Complete the function
